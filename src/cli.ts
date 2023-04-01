8e4a357f018c30c7a6bc09d01d68da88b143623d
spiff-radio <hello.spiff.radio@gmail.com>
2023-04-01 23:53:57 +0200

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -1,56 +1,59 @@
 #!/usr/bin/env node
 const clear = require('clear');
 const figlet = require('figlet');
-import {JSPFConverter} from "./types";
+
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import {JSPFPlaylist} from "./types";
 
 const jspf = {
-   "playlist" : {
-     "title"         : "JSPF example",
-     "creator"       : "Name of playlist author",
-     "annotation"    : "Super playlist",
-     "info"          : "http://example.com/",
-     "location"      : "http://example.com/",
-     "identifier"    : "http://example.com/",
-     "image"         : "http://example.com/",
-     "date"          : "2005-01-08T17:10:47-05:00",
-     "license"       : "http://example.com/",
-     "attribution"   : [
-       {"identifier"   : "http://example.com/"},
-       {"location"     : "http://example.com/"}
+   playlist : {
+     title         : "JSPF example"
+     ,
+     creator       : "Name of playlist author",
+     annotation    : "Super playlist",
+     info          : "http://example.com/",
+     location      : "http://example.com/",
+     identifier    : "http://example.com/",
+     image         : "http://example.com/",
+     license       : "http://example.com/",
+     attribution   : [
+       {identifier   : "http://example.com/"},
+       {location     : "http://example.com/"}
      ],
-     "link"          : [
+     link          : [
        {"http://example.com/rel/1/" : "http://example.com/body/1/"},
        {"http://example.com/rel/2/" : "http://example.com/body/2/"}
      ],
-     "meta"          : [
+     meta          : [
        {"http://example.com/rel/1/" : "my meta 14"},
        {"http://example.com/rel/2/" : "345"}
      ],
-     "extension"     : {
+     extension     : {
        "http://example.com/app/1/" : ['ARBITRARY_EXTENSION_BODY', 'ARBITRARY_EXTENSION_BODY'],
        "http://example.com/app/2/" : ['ARBITRARY_EXTENSION_BODY']
      },
-     "track"         : [
+     track         : [
        {
-         "location"      : ["http://example.com/1.ogg", "http://example.com/2.mp3"],
-         "identifier"    : ["http://example.com/1/", "http://example.com/2/"],
-         "title"         : "Track title",
-         "creator"       : "Artist name",
-         "annotation"    : "Some text",
-         "info"          : "http://example.com/",
-         "image"         : "http://example.com/",
-         "album"         : "Album name",
-         "trackNum"      : 1,
-         "duration"      : 0,
-         "link"          : [
+         location      : ["http://example.com/1.ogg", "http://example.com/2.mp3"],
+         identifier    : ["http://example.com/1/", "http://example.com/2/"],
+         title         : "Track title",
+         creator       : "Artist name",
+         annotation    : "Some text",
+         info          : "http://example.com/",
+         image         : "http://example.com/",
+         album         : "Album name",
+         trackNum      : 1,
+         duration      : 0,
+         link          : [
            {"http://example.com/rel/1/" : "http://example.com/body/1/"},
            {"http://example.com/rel/2/" : "http://example.com/body/2/"}
          ],
-         "meta"          : [
+         meta          : [
            {"http://example.com/rel/1/" : "my meta 14"},
            {"http://example.com/rel/2/" : "345"}
          ],
-         "extension"     : {
+         extension     : {
            "http://example.com/app/1/" : ['ARBITRARY_EXTENSION_BODY', 'ARBITRARY_EXTENSION_BODY'],
            "http://example.com/app/2/" : ['ARBITRARY_EXTENSION_BODY']
          }
@@ -59,26 +62,16 @@ const jspf = {
    }
  }
 
-/*
-const JSONTrackObj = JSON.stringify({
-  title         : "Track title",
-  creator       : "Artist name",
-  zob           : "zub",
-  trackNum: -12
-});
-
-const track = JSPFConverter.trackFromJSON(JSONTrackObj);
-*/
-
 clear();
 console.log(
   figlet.textSync('JSPF', { horizontalLayout: 'full' })
 );
+//const playlist = new JSPFPlaylist(jspf.playlist);
 
-const jspfObject = JSPFConverter.objectFromJSON(JSON.stringify(jspf));
-const playlist = jspfObject.playlist;
+const playlist = plainToClass(JSPFPlaylist, jspf.playlist, { excludeExtraneousValues: true })
+
+
 console.log(playlist);
-
-
+console.log(playlist.track[0].hello());
 
 console.log("END");
--- src/types.ts
+++ src/types.ts
@@ -1,84 +1,155 @@
 //https://github.com/metabrainz/listenbrainz-server/blob/cdc4df46738402d7d003acf08febfe7c7cf2fe4f/frontend/js/src/utils/types.d.ts
-import 'reflect-metadata';
-import {plainToClass,classToPlain} from "class-transformer";
+import {Type,Expose} from "class-transformer";
 
 
 export type URIT = string;
 
 export type JSPFAttributionT = Record<string,URIT>;
 export type JSPFLinkT = Record<URIT,URIT>;
-export type JSPFMetaT = Record<URIT,any>;
+//export type JSPFMetaT = Record<URIT,any>;
+export type JSPFMetaT = {
+  [url: string]: any;
+};
 export type JSPFExtensionT = Record<URIT,any>;
 
 export interface JSPFTrackT{
-  location?: URIT[],
-  identifier?: URIT[],
-  title?: string,
-  creator?: string,
-  annotation?: string,
-  info?: URIT,
-  image?: URIT,
-  album?: URIT,
-  trackNum?: number,//should be a non-negative integer
-  duration?: number,//should be a non-negative integer
-  link?: JSPFLinkT[],
-  meta?: JSPFMetaT[],
-  extension?: JSPFExtensionT
+  location: Set<URIT>,
+  identifier: URIT[],
+  title: string,
+  creator: string,
+  annotation: string,
+  info: URIT,
+  image: URIT,
+  album: URIT,
+  trackNum: number,//should be a non-negative integer
+  duration: number,//should be a non-negative integer
+  link: JSPFLinkT[],
+  meta: JSPFMetaT[],
+  extension: JSPFExtensionT
 };
 
 export interface JSPFPlaylistT{
-  title?: string,
-  creator?: string,
-  annotation?: string,
-  info?: URIT,
-  location?: URIT,
-  identifier?: URIT,
-  image?: URIT,
-  date?: Date, //DateTimeIso8601String
-  license?: URIT,
-  attribution?: JSPFAttributionT[],
-  link?: JSPFLinkT[],
-  meta?: JSPFMetaT[],
-  extension?: JSPFExtensionT,
-  track?: JSPFTrackT[]
+  title: string,
+  creator: string,
+  annotation: string,
+  info: URIT,
+  location: URIT,
+  identifier: URIT,
+  image: URIT,
+  date: Date, //DateTimeIso8601String
+  license: URIT,
+  attribution: JSPFAttributionT[],
+  link: JSPFLinkT[],
+  meta: JSPFMetaT[],
+  extension: JSPFExtensionT,
+  track: JSPFTrackT[]
 };
 
 export interface JSPFObjectT{
   playlist: JSPFPlaylistT
 };
 
-export class JSPFTrack implements JSPFTrackT{
-  location?: URIT[];
-  identifier?: URIT[];
-  title?: string;
-  creator?: string;
-  annotation?: string;
-  info?: URIT;
-  image?: URIT;
-  album?: URIT;
-  trackNum?: number;//should be a non-negative integer
-  duration?: number;//should be a non-negative integer
-  link?: JSPFLinkT[];
-  meta?: JSPFMetaT[];
-  extension?: JSPFExtensionT;
+export class JSPFTrack{
+
+  @Expose()
+  location: Set<URIT>;
+
+  @Expose()
+  identifier: Set<URIT>;
+
+  @Expose()
+  title: string;
+
+  @Expose()
+  creator: string;
+
+  @Expose()
+  annotation: string;
+
+  @Expose()
+  info: URIT;
+
+  @Expose()
+  image: URIT;
+
+  @Expose()
+  album: URIT;
+
+  @Expose()
+  trackNum: number;//should be a non-negative integer
+
+  @Expose()
+  duration: number;//should be a non-negative integer
+
+  @Expose()
+  link: JSPFLinkT[];
+
+  @Expose()
+  meta: JSPFMetaT[];
+
+  @Expose()
+  extension: JSPFExtensionT;
+
+  constructor(partial: Partial<JSPFTrack>) {
+    console.log("ZOB")
+    Object.assign(this, partial);
+  }
+
+  hello(){
+    return "TRACK YO"
+  }
 
 }
 
-export class JSPFPlaylist implements JSPFPlaylistT {
-  title?: string;
-  creator?: string;
-  annotation?: string;
-  info?: URIT;
-  location?: URIT;
-  identifier?: URIT;
-  image?: URIT;
-  date?: Date; //DateTimeIso8601String
-  license?: URIT;
-  attribution?: JSPFAttributionT[];
-  link?: JSPFLinkT[];
-  meta?: JSPFMetaT[];
-  extension?: JSPFExtensionT;
-  track?: JSPFTrackT[];
+export class JSPFPlaylist {
+  @Expose()
+  title: string;
+
+  @Expose()
+  creator: string;
+
+  @Expose()
+  annotation: string;
+
+  @Expose()
+  info: URIT;
+
+  @Expose()
+  location: URIT;
+
+  @Expose()
+  identifier: URIT;
+
+  @Expose()
+  image: URIT;
+
+  @Type(() => Date)
+  @Expose()
+  date: Date; //DateTimeIso8601String
+
+  @Expose()
+  license: URIT;
+
+  @Expose()
+  attribution: JSPFAttributionT[];
+
+  @Expose()
+  link: JSPFLinkT[];
+
+  @Expose()
+  meta: JSPFMetaT[];
+
+  @Expose()
+  extension: JSPFExtensionT;
+
+  @Type(() => JSPFTrack)
+  @Expose()
+  track: JSPFTrack[];
+
+  constructor(partial: Partial<JSPFPlaylist>) {
+    Object.assign(this, partial);
+  }
+
   public addTracks(tracks:JSPFTrack[]) {
   }
   public removeTracks(tracks:JSPFTrack[]) {
@@ -89,34 +160,5 @@ export class JSPFPlaylist implements JSPFPlaylistT {
   }
   public hello():string {
     return 'hello ' + this.title;
-  }
-}
-
-export class JSPFObject implements JSPFObjectT {
-  playlist:JSPFPlaylistT
-}
-
-//convert JSON to classes & vice-versa
-export class JSPFConverter{
-  public static objectFromJSON(json: string){
-    const obj = JSON.parse(json);
-    return plainToClass(JSPFObject,obj)
-  }
-  public static JSONFromObject(object: JSPFObjectT){
-    return classToPlain(object);
-  }
-  public static playlistFromJSON(json: string){
-    const obj = JSON.parse(json);
-    return plainToClass(JSPFPlaylist,obj)
-  }
-  public static JSONFromPlaylist(playlist: JSPFPlaylistT){
-    return classToPlain(playlist)
-  }
-  public static trackFromJSON(json: string){
-    const obj = JSON.parse(json);
-    return plainToClass(JSPFTrack,obj)
-  }
-  public static JSONFromTrack(track: JSPFTrackT){
-    return classToPlain(track)
   }
 }
